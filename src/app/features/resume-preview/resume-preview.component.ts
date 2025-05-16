90981212d76c840889a796707d9c59d07c57281a
Abheek03 <57488408+Abheek03@users.noreply.github.com>
2025-05-16 15:49:58 +0000

    added the components

---
+++ src/app/features/resume-preview/resume-preview.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resume-preview',
+  imports: [],
+  templateUrl: './resume-preview.component.html',
+  styleUrl: './resume-preview.component.css'
+})
+export class ResumePreviewComponent {
+
+}

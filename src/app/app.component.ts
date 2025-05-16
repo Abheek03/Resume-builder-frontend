464d8e881d8a9e6e3f7c835a4bb7905dc2bec6b3
Abheek03 <57488408+Abheek03@users.noreply.github.com>
2025-05-16 18:09:08 +0000

    added routing to the components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthComponent } from './features/auth/auth.component';
+import { ResumeEditorComponent } from './features/resume-editor/resume-editor.component';
+import { ResumePreviewComponent } from './features/resume-preview/resume-preview.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AuthComponent,ResumeEditorComponent,ResumePreviewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
